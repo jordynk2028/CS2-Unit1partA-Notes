401362599d60c3c2ef12d9a21930e88872d13bc2
jordynk2028 <jordyn.kesselman2028@gbwl.org>
2026-09-23 18:36:19 +0000

    added more notes

--- Main.java
+++ Main.java
@@ -6,3 +6,23 @@ public class Main {
       System.out.println("Hello World");
    }
 }
+
+/* - primitive type- storing information/data(simple)
+- object (refrnace) type-storing complex data and objects
+
+Primitive avrible typew:
+1. int.- store integers/positive or negitive whole numbers
+2. dubble-store decimal numbers 
+3.boolean- stores logic (true or false)
+
+
+Object varible type:
+1.String - stores text 
+
+
+setting up varibles 
+1. declare - int. x (gives name no info)
+2. Assighn - x = 5; "Ms.Dinko" (gives the info)
+
+Initialize- int x = 5
+ */
